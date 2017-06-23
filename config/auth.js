9fdf77c562f28e07e600e701859df467e9908c56
Brandon Hogan <me@brandonhogan.com>
2017-06-23 15:39:59 -0400

    added google auth id and secret

--- config/auth.js
+++ config/auth.js
@@ -14,8 +14,8 @@ module.exports = {
     },
 
     'googleAuth' : {
-        'clientID'      : 'your-secret-clientID-here',
-        'clientSecret'  : 'your-client-secret-here',
+        'clientID'      : '  844062822101-c6f68972dkft9mbk01q3mvbnhl8ieu27.apps.googleusercontent.com ',
+        'clientSecret'  : ' NuHaPLbT65OJTu9eN6qKfvMA ',
         'callbackURL'   : 'http://localhost:8080/auth/google/callback'
     }
 
